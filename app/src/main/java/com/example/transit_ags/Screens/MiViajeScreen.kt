1aa137e7afc97743ae5f32b7d58ca3a869bec7f4
Sofia Gonzalez <miroslava299064@gmail.com>
2025-03-28 16:58:20 -0600

    Actualizacion de la BD

---
+++ app/src/main/java/com/example/transit_ags/Screens/MiViajeScreen.kt
@@ -0,0 +1,24 @@
+package com.example.transit_ags.Screens
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
+import androidx.navigation.NavController
+
+@Composable
+fun MiViajeScreen(navController: NavController){
+    Column{
+        val context = LocalContext.current
+
+        Text(text = "ESTE VA HACER LA VENTANA DE CONSULTAR EL SALDO DE YO VOY")
+
+        Button(onClick = {navController.navigate("recargamap")}){ Text(text = "PUNTOS DE RECARGA") }
+        Button(onClick = {navController.navigate("recargarahora")}){ Text(text = "RECAGAR AHORA EN LINEA") }
+
+        Button(onClick = {navController.navigate("cuenta")}){ Text(text = "REGRESAR") }
+
+    }
+
+}
